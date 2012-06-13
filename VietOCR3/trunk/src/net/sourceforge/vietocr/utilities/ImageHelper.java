1eb0052afce95e3dd7dd2247c688299e2e8c61db
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2012-06-13 04:35:21 +0000

    Add a couple more convenient methods

--- VietOCR3/trunk/src/net/sourceforge/vietocr/utilities/ImageHelper.java
+++ VietOCR3/trunk/src/net/sourceforge/vietocr/utilities/ImageHelper.java
@@ -1,17 +1,17 @@
 /**
  * Copyright @ 2008 Quan Nguyen
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package net.sourceforge.vietocr.utilities;
 
@@ -23,18 +23,16 @@ import java.awt.Transparency;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.image.BufferedImage;
+import javax.imageio.IIOImage;
 
 public class ImageHelper {
 
     /**
-     * Convenience method that returns a scaled instance of the
-     * provided {@code BufferedImage}.
+     * Convenience method that returns a scaled instance of the provided {@code BufferedImage}.
      *
      * @param image the original image to be scaled
-     * @param targetWidth the desired width of the scaled instance,
-     *    in pixels
-     * @param targetHeight the desired height of the scaled instance,
-     *    in pixels
+     * @param targetWidth the desired width of the scaled instance, in pixels
+     * @param targetHeight the desired height of the scaled instance, in pixels
      * @return a scaled version of the original {@code BufferedImage}
      */
     public static BufferedImage getScaledInstance(BufferedImage image, int targetWidth, int targetHeight) {
@@ -49,14 +47,39 @@ public class ImageHelper {
     }
 
     /**
-     * A replacement for the standard <code>BufferedImage.getSubimage</code> method.
+     * Convenience method that returns a scaled instance of the provided {@code IIOImage}.
      * 
+     * @param iioSource the original image to be scaled
+     * @param scale the desired scale
+     * @return a scaled version of the original {@code IIOImage}
+     */
+    public static IIOImage getScaledInstance(IIOImage iioSource, float scale) {
+        if (!(iioSource.getRenderedImage() instanceof BufferedImage)) {
+            throw new IllegalArgumentException("RenderedImage in IIOImage must be BufferedImage");
+        }
+
+        if (scale == 1.0) {
+            return iioSource;
+        }
+        
+        BufferedImage source = (BufferedImage) iioSource.getRenderedImage();
+        BufferedImage target = getScaledInstance(source, (int) (scale * source.getWidth()), (int) (scale * source.getHeight()));
+        return new IIOImage(target, null, null);
+    }
+
+    /**
+     * A replacement for the standard
+     * <code>BufferedImage.getSubimage</code> method.
+     *
      * @param image
-     * @param x the X coordinate of the upper-left corner of the specified rectangular region
-     * @param y the Y coordinate of the upper-left corner of the specified rectangular region
+     * @param x the X coordinate of the upper-left corner of the specified
+     * rectangular region
+     * @param y the Y coordinate of the upper-left corner of the specified
+     * rectangular region
      * @param width the width of the specified rectangular region
      * @param height the height of the specified rectangular region
-     * @return a BufferedImage that is the subimage of <code>image</code>.
+     * @return a BufferedImage that is the subimage of
+     * <code>image</code>.
      */
     public static BufferedImage getSubImage(BufferedImage image, int x, int y, int width, int height) {
         int type = (image.getTransparency() == Transparency.OPAQUE)
@@ -64,6 +87,20 @@ public class ImageHelper {
         BufferedImage tmp = new BufferedImage(width, height, type);
         Graphics2D g2 = tmp.createGraphics();
         g2.drawImage(image.getSubimage(x, y, width, height), 0, 0, null);
+        g2.dispose();
+        return tmp;
+    }
+
+    /**
+     * A simple method to convert an image to binary or B/W image.
+     *
+     * @param image Input image
+     * @return
+     */
+    public static BufferedImage convertImage2Binary(BufferedImage image) {
+        BufferedImage tmp = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        Graphics2D g2 = tmp.createGraphics();
+        g2.drawImage(image, 0, 0, null);
         g2.dispose();
         return tmp;
     }
