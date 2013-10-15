d8684f9275eb26ca3fdbb3c320485838f5b81849
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2013-10-15 00:45:02 +0000

    Add unit test for FixedSizeStack

---
+++ VietOCR3/trunk/test/net/sourceforge/vietocr/utilities/FixedSizeStackTest.java
@@ -0,0 +1,46 @@
+package net.sourceforge.vietocr.utilities;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FixedSizeStackTest {
+    
+    public FixedSizeStackTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of push method, of class FixedSizeStack.
+     */
+    @Test
+    public void testPush() {
+        System.out.println("push");
+        String expected = "9";
+        FixedSizeStack<String> instance = new FixedSizeStack<String>(5);
+        for (int i = 0; i < 10; i++) {
+            instance.push(String.valueOf(i));
+        }
+        
+        String actual = instance.pop();
+        assertEquals(expected, actual);
+    }
+}
