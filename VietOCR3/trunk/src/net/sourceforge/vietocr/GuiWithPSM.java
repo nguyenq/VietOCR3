7bcb174eb8179de86ba25c744e5478cb354b88a1
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2014-11-26 02:06:12 +0000

    Make PageSegMode name more accessible

--- VietOCR3/trunk/src/net/sourceforge/vietocr/GuiWithPSM.java
+++ VietOCR3/trunk/src/net/sourceforge/vietocr/GuiWithPSM.java
@@ -21,48 +21,18 @@ import javax.swing.ButtonGroup;
 import javax.swing.JRadioButtonMenuItem;
 
 public class GuiWithPSM extends GuiWithBatch {
-
-    public enum PageSegMode {
-
-        PSM_OSD_ONLY("0", "0 - Orientation and script detection (OSD) only"),
-        PSM_AUTO_OSD("1", "1 - Automatic page segmentation with OSD"),
-        PSM_AUTO_ONLY("2", "2 - Automatic page segmentation, but no OSD, or OCR"),
-        PSM_AUTO("3", "3 - Fully automatic page segmentation, but no OSD (default)"),
-        PSM_SINGLE_COLUMN("4", "4 - Assume a single column of text of variable sizes"),
-        PSM_SINGLE_BLOCK_VERT_TEXT("5", "5 - Assume a single uniform block of vertically aligned text"),
-        PSM_SINGLE_BLOCK("6", "6 - Assume a single uniform block of text"),
-        PSM_SINGLE_LINE("7", "7 - Treat the image as a single text line"),
-        PSM_SINGLE_WORD("8", "8 - Treat the image as a single word"),
-        PSM_CIRCLE_WORD("9", "9 - Treat the image as a single word in a circle"),
-        PSM_SINGLE_CHAR("10", "10 - Treat the image as a single character");
-        private final String val;
-        private final String desc;
-
-        private PageSegMode(String val, String desc) {
-            this.val = val;
-            this.desc = desc;
-        }
-
-        public String getVal() {
-            return val;
-        }
-
-        public String getDesc() {
-            return desc;
-        }
-    }
     private final String strPSM = "PageSegMode";
 
     public GuiWithPSM() {
         selectedPSM = prefs.get(strPSM, "3");
-        this.jLabelPSMvalue.setText(selectedPSM);
+        this.jLabelPSMvalue.setText(enumOf(selectedPSM));
 
         ActionListener psmLst = new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent ae) {
                 selectedPSM = ae.getActionCommand();
-                jLabelPSMvalue.setText(selectedPSM);
+                jLabelPSMvalue.setText(enumOf(selectedPSM));
             }
         };
 
@@ -75,6 +45,16 @@ public class GuiWithPSM extends GuiWithBatch {
             groupPSM.add(radioItem);
             this.jMenuPSM.add(radioItem);
         }
+    }
+
+    /**
+     * Gets accessible name of PageSegMode enum.
+     * 
+     * @param val
+     * @return 
+     */
+    final String enumOf(String val) {
+        return PageSegMode.enumOf(val).name().replace("PSM_", "").replace("_", " ");
     }
 
     @Override
