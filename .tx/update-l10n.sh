f6c4157f920b723c20bf3a9e8b1710a617ffe326
Rimas Kudelis <rimas@kudelis.lt>
2024-03-05 11:05:55 +0200

    Remove unused settings from .tx/config; improve the update script

--- .tx/update-l10n.sh
+++ .tx/update-l10n.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-tx pull -a -f --minimum-perc 75
-for i in `find . -name '*_??.properties' | grep -v '_vi\.properties'`; do native2ascii -encoding ISO-8859-1 $i $i.tmp; mv $i.tmp $i; done
+tx pull --all --force --minimum-perc 75 --mode onlytranslated
+# Re-pull readme files with untranslated strings in English (instead of omitted)
+tx pull --translations --force --minimum-perc 75 vietocr.readme_html
+for i in `find . -name '*_??.properties' | grep -v '_vi\.properties'`; do
+  native2ascii -encoding ISO-8859-1 "$i" "$i.tmp"
+  awk '!/^[^=]+=$/' "$i.tmp" > "$i"
+  rm "$i.tmp"
+done
